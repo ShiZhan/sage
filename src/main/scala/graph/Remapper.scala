e56e4c6944b8bfe903e43f27f828039919dd5cea
ShiZhan <g.shizhan.g@gmail.com>
2015-04-30 12:24:59 +0800

    remove remapper logger, purely output edges.

--- src/main/scala/graph/Remapper.scala
+++ src/main/scala/graph/Remapper.scala
@@ -1,29 +1,27 @@
 package graph
 
-class Remapper(mapFN: String) extends helper.Logging {
+class Remapper(mapFN: String) {
   import EdgeUtils.line2edge
   import helper.GetLines
 
   private val vMap = GetLines.fromFile(mapFN)
     .zipWithIndex.map { case (str, idx) => (str.toLong, idx.toLong) }.toMap
 
-  def remapCSV(edgeFN: String) = {
-    logger.info("remapping CSV [{}]", edgeFN)
-    val edges = GetLines.fromFileOrConsole(edgeFN).map(line2edge).filter(_.valid)
-    val edgesMapped = edges.map { case Edge(u, v) => Edge(vMap.getOrElse(u, u), vMap.getOrElse(v, v)) }
-    edgesMapped.foreach(println)
+  private def mapEdge(e: Edge) = e match {
+    case Edge(u, v) => Edge(vMap.getOrElse(u, u), vMap.getOrElse(v, v))
   }
 
+  def remapCSV(edgeFN: String) =
+    GetLines.fromFileOrConsole(edgeFN)
+      .map(line2edge).filter(_.valid)
+      .map(mapEdge).foreach(println)
+
   def remapBIN(shardFN: String) = {
-    logger.info("remapping BIN [{}]", shardFN)
-    val edges = Shard(shardFN).getEdges
-    val edgesMapped = edges.map { case Edge(u, v) => Edge(vMap.getOrElse(u, u), vMap.getOrElse(v, v)) }
-    val shardMapped = Shard(shardFN + "-mapped.bin")
-    shardMapped.putEdges(edgesMapped)
+    val edgesMapped = Shard(shardFN).getEdges.map(mapEdge)
+    Shard(shardFN + "-mapped.bin").putEdges(edgesMapped)
   }
 }
 
 object Remapper {
-  def run(inFile: String, mapFile: String) =
-    new Remapper(mapFile).remapCSV(inFile)
+  def run(edgeFile: String, mapFile: String) = new Remapper(mapFile).remapCSV(edgeFile)
 }
