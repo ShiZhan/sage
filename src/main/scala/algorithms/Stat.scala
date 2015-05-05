616a776de8c3b4dda432635e1650c454dbbaa890
ShiZhan <g.shizhan.g@gmail.com>
2015-05-05 23:32:04 +0800

    TODO: graph stat

---
+++ src/main/scala/algorithms/Stat.scala
@@ -0,0 +1,8 @@
+package algorithms
+
+import graph.{ Edge, Vertices, Shards }
+
+class Stat(vertices: Vertices, shards: Shards) {
+  def run = {
+  }
+}
--- src/main/scala/graph/Processor.scala
+++ src/main/scala/graph/Processor.scala
@@ -11,9 +11,10 @@ object Processor {
         case "bfs" :: root :: Nil => new BFS(vertices, shards).run(root.toLong)
         case "sssp" :: root :: Nil => new SSSP(vertices, shards).run(root.toLong)
         case "cc" :: Nil => new CC(vertices, shards).run
-        case "scc" :: Nil =>
-        case "pagerank" :: Nil =>
-        case "triangle" :: Nil =>
+        case "scc" :: Nil => new SCC(vertices, shards).run
+        case "pagerank" :: Nil => new PageRank(vertices, shards).run
+        case "triangle" :: Nil => new Triangle(vertices, shards).run
+        case "stat" :: Nil => new Stat(vertices, shards).run
         case _ => shards.getAllEdges.foreach(println)
       }
       vertices.close()
