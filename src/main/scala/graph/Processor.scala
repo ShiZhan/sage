d24547fc2024bb78dd3450c78a0cd336b2227c62
ShiZhan <g.shizhan.g@gmail.com>
2015-05-06 17:45:39 +0800

    usage

--- src/main/scala/graph/Processor.scala
+++ src/main/scala/graph/Processor.scala
@@ -11,7 +11,6 @@ object Processor {
         case "bfs" :: root :: Nil => new BFS(vertices, shards).run(root.toLong)
         case "sssp" :: root :: Nil => new SSSP(vertices, shards).run(root.toLong)
         case "cc" :: Nil => new CC(vertices, shards).run
-        case "scc" :: Nil => new SCC(vertices, shards).run
         case "cluster" :: Nil => new Cluster(vertices, shards).run
         case "pagerank" :: Nil => new PageRank(vertices, shards).run
         case "triangle" :: Nil => new Triangle(vertices, shards).run
