3ff1533a434fe06c911ed9f43fbe38b0427fb4e5
ShiZhan <g.shizhan.g@gmail.com>
2015-05-08 16:18:32 +0800

    Timing module for future use

---
+++ src/main/scala/helper/Timing.scala
@@ -0,0 +1,28 @@
+/**
+ * Timer
+ */
+package common
+
+/**
+ * @author ShiZhan
+ * Timer
+ */
+object Timing {
+  implicit class TimerWrapper[T](op: () => T) {
+    def elapsed = {
+      val t1 = compat.Platform.currentTime
+      val result = op()
+      val t2 = compat.Platform.currentTime
+      (result, t2 - t1)
+    }
+  }
+
+  implicit class TimerWrapperUnit(op: () => Unit) {
+    def elapsed = {
+      val t1 = compat.Platform.currentTime
+      op()
+      val t2 = compat.Platform.currentTime
+      t2 - t1
+    }
+  }
+}
