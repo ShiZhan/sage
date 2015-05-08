8f416f7e0ecd7630cfb16627509d28ffa6b84996
ShiZhan <g.shizhan.g@gmail.com>
2015-05-08 16:17:38 +0800

    adjust Processer design

---
+++ src/main/scala/algorithms/Community.scala
@@ -0,0 +1,10 @@
+package algorithms
+
+import graph.{ Edge, Vertices, Shards }
+
+class Community(shards: Shards) {
+  val vertices = new Vertices[Long]("")
+
+  def run = {
+  }
+}
--- src/main/scala/graph/Processor.scala
+++ src/main/scala/graph/Processor.scala
@@ -10,11 +10,13 @@ object Processor {
         case "bfs" :: root :: Nil => new BFS(shards).run(root.toLong)
         case "sssp" :: root :: Nil => new SSSP(shards).run(root.toLong)
         case "cc" :: Nil => new CC(shards).run
+        case "community" :: Nil => new Community(shards).run
         case "cluster" :: Nil => new Cluster(shards).run
         case "pagerank" :: Nil => new PageRank(shards).run
         case "triangle" :: Nil => new Triangle(shards).run
         case "stat" :: Nil => new Stat(shards).run
-        case _ => shards.getAllEdges.foreach(println)
+        case "print" :: Nil => shards.getAllEdges.foreach(println)
+        case _ => println(s"[$algorithm] not implemented")
       }
     } else println("edge list(s) incomplete")
   }
