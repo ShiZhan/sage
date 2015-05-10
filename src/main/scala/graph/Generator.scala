e629edeec84f195ae1eea4e1c110753a6d742d70
ShiZhan <g.shizhan.g@gmail.com>
2015-05-10 17:50:21 +0800

    avoid self loop & nearest neighbours

--- src/main/scala/graph/Generator.scala
+++ src/main/scala/graph/Generator.scala
@@ -69,7 +69,7 @@ class ErdosRenyi(scale: Int, ratio: Double) {
 
 class SmallWorld(scale: Int, neighbour: Int, rewiring: Double) {
   require(scale > 0
-    && neighbour > 0 && neighbour < (1L << scale)
+    && neighbour > 0 && neighbour < (1L << (scale - 1))
     && rewiring < 1 && rewiring > 0)
   import scala.util.Random
 
@@ -82,11 +82,9 @@ class SmallWorld(scale: Int, neighbour: Int, rewiring: Double) {
     Iterator.continually { vID += 1; vID }.takeWhile(_ < total)
   }
 
-  def rewire = Random.nextLong & ((total >> 1) - 1) + neighbour + 1
-
   def neighbours(id: Long) = (1 to neighbour).toIterator.map { n =>
     if (Random.nextInt(range) < probability)
-      (id + rewire) & (total - 1)
+      (id + Random.nextLong) & (total - 1)
     else
       (id + n) & (total - 1)
   }
