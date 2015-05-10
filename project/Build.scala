9ff2544602add4d1ff8a81d5e9abc39a61c925bf
ShiZhan <g.shizhan.g@gmail.com>
2015-05-10 22:09:29 +0800

    ...

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ object MyBuild extends Build {
   lazy val buildSettings = Defaults.defaultSettings ++ Seq(
     version := "0.1-SNAPSHOT",
     organization := "com.simba",
-    scalaVersion := Option(System.getProperty("scala.version")).getOrElse("2.11.6")
+    scalaVersion := "2.11.6"
   )
 
   lazy val sage = Project(
