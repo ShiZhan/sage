aa207b614eeb4824586147978be193162a9014b0
ShiZhan <g.shizhan.g@gmail.com>
2015-05-12 14:18:39 +0800

    calculate degree

--- src/main/scala/algorithms/Stat.scala
+++ src/main/scala/algorithms/Stat.scala
@@ -1,12 +1,24 @@
 package algorithms
 
 import graph.{ Edge, Vertices, Shards }
-
-case class VertexStat(degree: Int, core: Int)
+import helper.Gauge.IteratorOperations
 
 class Stat(shards: Shards) {
-  val vertices = new Vertices[VertexStat]("")
+  case class V(degree: Int, core: Int)
+  val vertices = new Vertices[V]("")
 
   def run = {
+    val data = vertices.data
+    shards.getAllEdges.foreachDo { case Edge(u, v) =>
+      Seq(u, v).foreach { k =>
+        if (data.containsKey(k)) {
+          val V(d, c) = data.get(k)
+          data.put(k, V(d + 1, c))
+        } else {
+          data.put(k, V(1, 1))
+        }
+      }
+    }
+    vertices.print
   }
 }
