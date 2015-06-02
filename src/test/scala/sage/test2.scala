71dee8f849427fb01a623c3a685c58490d2cddba
ShiZhan <g.shizhan.g@gmail.com>
2015-06-03 00:52:31 +0800

    buffered access testing

--- src/test/scala/sage/test2.scala
+++ src/test/scala/sage/test2.scala
@@ -34,3 +34,81 @@ object EdgeSort {
     println("Seq.sorted time cost: " + e2 + " ms")
   }
 }
+
+object FileBufAccessTest {
+  import java.nio.{ ByteBuffer, ByteOrder }
+  import java.nio.channels.FileChannel
+  import java.nio.file.Paths
+  import java.nio.file.StandardOpenOption._
+  import graph.Edge
+  import graph.Edges.edgeScale
+  import helper.Timing._
+  import helper.Gauge.IteratorOperations
+
+  def edges = {
+    var i = -1L
+    Iterator.continually { i += 1; Edge(i, i + 1) }.take(1 << 20)
+  }
+
+  def bufferedReadWrite(fc: FileChannel, scale: Int) = {
+    val size = 1 << scale
+    val gSize = 1 << (scale - edgeScale)
+    val buf = ByteBuffer.allocate(size).order(ByteOrder.LITTLE_ENDIAN)
+    val ew = {
+      () =>
+        {
+          fc.position(0)
+          edges.grouped(gSize).foreach { g =>
+            buf.clear()
+            for (Edge(u, v) <- g) { buf.putLong(u); buf.putLong(v) }
+            buf.flip()
+            while (buf.hasRemaining) fc.write(buf)
+          }
+        }
+    }.elapsed
+    println("%9d buffer write: %9d ms".format(size, ew))
+    val length = fc.size()
+    fc.position(0)
+    val er = {
+      () =>
+        {
+          while (fc.position < length) {
+            buf.clear()
+            while (fc.read(buf) != -1 && buf.hasRemaining) {}
+            buf.flip()
+            while (buf.hasRemaining) {
+              val u = buf.getLong
+              val v = buf.getLong
+              //Edge(u, v)
+            }
+          }
+        }
+    }.elapsed
+    println("%9d buffer read:  %9d ms".format(size, er))
+  }
+
+  def show(fc: FileChannel) = {
+    val buf = ByteBuffer.allocate(1 << 17).order(ByteOrder.LITTLE_ENDIAN)
+    val length = fc.size()
+    fc.position(0)
+    while (fc.position < length) {
+      buf.clear()
+      while (fc.read(buf) != -1 && buf.hasRemaining) {}
+      buf.flip()
+      while (buf.hasRemaining) {
+        val u = buf.getLong
+        val v = buf.getLong
+        print(Edge(u, v) + "\r")
+      }
+    }
+  }
+
+  def main(args: Array[String]) = {
+    val name = "test.bin"
+    val p = Paths.get(name)
+    val fc = FileChannel.open(p, READ, WRITE, CREATE)
+    (edgeScale to 17).foreach { bufferedReadWrite(fc, _) }
+    show(fc)
+    fc.close()
+  }
+}
