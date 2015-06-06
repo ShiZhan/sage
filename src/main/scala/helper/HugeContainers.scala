c04afda149664bf8066ae7041ba4e6efdd7f25e9
ShiZhan <g.shizhan.g@gmail.com>
2015-06-06 19:30:40 +0800

    strange performance degrade

--- src/main/scala/helper/HugeContainers.scala
+++ src/main/scala/helper/HugeContainers.scala
@@ -11,29 +11,46 @@ package helper
  * LargeBitSet:   BitSet indexed by Long
  */
 object HugeContainers {
+  object MaxSize {
+    import JStates.MEM
+
+    private val pow2values = (0 to 62).map(1L << _)
+    private def nextPow2(l: Long) = pow2values.find(_ > l)
+    private def prevPow2(l: Long) = pow2values.reverse.find(_ < l)
+    val forLong = prevPow2(MEM.MAX) match { case Some(s) => s >> 3; case _ => -1 }
+    val forInt = prevPow2(MEM.MAX) match { case Some(s) => s >> 2; case _ => -1 }
+    val forByte = prevPow2(MEM.MAX) match { case Some(s) => s; case _ => -1 }
+  }
+
   class GrowingArray[T: Manifest](v: T) {
     import scala.collection.mutable.ArrayBuffer
     import GrowingArray.Const._
+
     val data = ArrayBuffer.fill(1)(Array.fill(sizeRow)(v))
     private def more(n: Int) = (1 to n).foreach { i => data += Array.fill(sizeRow)(v) }
-    def get(index: Long) = { // 0..21|22..43, index < (1 << 43)
+
+    def apply(index: Long) = { // 0..20|21..51, index < (1 << 52)
+      val row = data.size
       val high = (index >> scaleRow).toInt
-      if (high >= data.size) more(high - data.size + 1)
+      if (high >= row) more(high - row + 1)
       val low = (index & maskRow).toInt
       data(high)(low)
     }
-    def put(index: Long, d: T) = {
+
+    def apply(index: Long, d: T) = {
+      val row = data.size
       val high = (index >> scaleRow).toInt
-      if (high >= data.size) more(high - data.size + 1)
+      if (high >= row) more(high - row + 1)
       val low = (index & maskRow).toInt
       data(high)(low) = d
     }
+
     def size = data.size.toLong << scaleRow
   }
 
   object GrowingArray {
     object Const {
-      val scaleRow = 21
+      val scaleRow = 20
       val sizeRow = 1 << scaleRow
       val maskRow = sizeRow - 1
     }
@@ -42,9 +59,10 @@ object HugeContainers {
   }
 
   class MaxArray[T: Manifest](v: T) {
-    import MaxArray.maxSize
-    require(maxSize > 0 && maxSize < Int.MaxValue)
-    val data = Array.fill(maxSize.toInt)(v)
+    import MaxSize.{ forLong => MaxLong }
+    require(MaxLong > 0 && MaxLong < Int.MaxValue)
+
+    val data = Array.fill(MaxLong.toInt)(v)
     def get(index: Long) = data(index.toInt)
     def put(index: Long, d: T) = data(index.toInt) = d
     def unused(index: Long) = data(index.toInt) == v
@@ -54,12 +72,6 @@ object HugeContainers {
   }
 
   object MaxArray {
-    import JStates.MEM
-    val pow2s = (0 to 62).map(1L << _)
-    def nextPow2(l: Long) = pow2s.find(_ > l)
-    def prevPow2(l: Long) = pow2s.reverse.find(_ < l)
-    val maxSize = prevPow2(MEM.MAX) match { case Some(s) => s >> 3; case _ => -1 }
-
     def apply[T: Manifest](v: T) = new MaxArray[T](v)
   }
 
@@ -67,15 +79,18 @@ object HugeContainers {
     import FlatArray.{ Const, split }
     val (s2, s1, s0) = split(size)
     require(s2 > 0 || s1 > 0 || s0 > 0)
+
     val d2 = if (s1 == 0 && s0 == 0) s2 else s2 + 1
     val d1 = if (s2 == 0) { if (s0 == 0) s1 else s1 + 1 } else Const.size1
     val d0 = if (s1 == 0 && s2 == 0) s0 else Const.size0
     val data = Array.fill(d2, d1, d0)(default)
+
     def apply(index: Long) = {
       val (i2, i1, i0) = split(index)
       data(i2)(i1)(i0)
     }
-    def update(index: Long, v: T) = {
+
+    def apply(index: Long, v: T) = {
       val (i2, i1, i0) = split(index)
       data(i2)(i1)(i0) = v
     }
@@ -83,12 +98,12 @@ object HugeContainers {
 
   object FlatArray {
     object Const {
-      val scale0 = 22
-      val scale1 = 44
-      val size0 = 1 << 22
-      val size1 = 1 << 22
-      val mask0 = (1L << 22) - 1L
-      val mask1 = (1L << 22) - 1L
+      val scale0 = 20
+      val scale1 = 40
+      val size0 = 1 << 20
+      val size1 = 1 << 20
+      val mask0 = size0 - 1L
+      val mask1 = size1 - 1L
     }
 
     def split(i: Long) = (
@@ -101,6 +116,7 @@ object HugeContainers {
   class LargeBitSet {
     import LargeBitSet.Const.{ maskLong, sizeLong }
     import LargeBitSet.countBits
+
     val data = Array.fill(sizeLong)(0L)
     def get(index: Long) = { (data((index >> 6).toInt) & (1 << (index & maskLong))) != 0 }
     def set(index: Long) = { data((index >> 6).toInt) |= (1 << (index & maskLong)) }
--- src/test/scala/sage/ContainerTest.scala
+++ src/test/scala/sage/ContainerTest.scala
@@ -1,7 +1,7 @@
 package sage.test.HugeContainers
 
 object GrowingArrayTest {
-  import helper.HugeContainers.{ GrowingArray, MaxArray }
+  import helper.HugeContainers.{ GrowingArray, MaxSize }
   import helper.Timing._
 
   def main(args: Array[String]) = {
@@ -9,20 +9,20 @@ object GrowingArrayTest {
     val sizeB = 1 << 21
     val maskB = sizeB - 1
     println(s"---\nfirst [0 .. $maskB]")
-    (0 to maskB).foreach { i => ha.put(i, i) }
-    val head0 = ha.get(0); val tail0 = ha.get(maskB)
+    (0 to maskB).foreach { i => ha(i, i) }
+    val head0 = ha(0); val tail0 = ha(maskB)
     val size0 = ha.size
     println(s"$head0 .. $tail0, $size0")
     println(s"---\nnext  [0 .. $maskB]")
-    (0 to maskB).map(_ + sizeB).foreach { i => ha.put(i, i) }
-    val head1 = ha.get(sizeB); val tail1 = ha.get(sizeB + maskB)
+    (0 to maskB).map(_ + sizeB).foreach { i => ha(i, i) }
+    val head1 = ha(sizeB); val tail1 = ha(sizeB + maskB)
     val size1 = ha.size
     println(s"$head1 .. $tail1, $size1")
-    val total = MaxArray.maxSize.toInt
-    val e = { () => (0 to (total - 1)).reverse.foreach { i => ha.put(i, total - i) } }.elapsed
+    val total = MaxSize.forLong.toInt
+    val e = { () => (0 to (total - 1)).reverse.foreach { i => ha(i, total - i) } }.elapsed
     val size2 = ha.size
     println(s"---\n$size2 elements ...")
-    val head2 = ha.get(0L); val tail2 = ha.get(total - 1)
+    val head2 = ha(0L); val tail2 = ha(total - 1)
     println(s"$head2 .. $tail2")
     println(s"$e ms")
   }
@@ -41,13 +41,13 @@ object MaxArrayTest {
 }
 
 object FlatArrayTest {
-  import helper.HugeContainers.{ FlatArray, MaxArray }
+  import helper.HugeContainers.{ FlatArray, MaxSize }
   import helper.Timing._
 
   def main(args: Array[String]) = {
-    val total = MaxArray.maxSize.toInt + 1
-    val fa = FlatArray[Int](total, Int.MaxValue)
-    val e = { () => (0 to (total - 1)).reverse.foreach { i => fa.update(i, total - i) } }.elapsed
+    val total = MaxSize.forLong.toInt + 1
+    val fa = FlatArray[Long](total, -1L)
+    val e = { () => (0 to (total - 1)).reverse.foreach { i => fa(i, total - i) } }.elapsed
     println(s"expected $total elements, $e ms")
   }
 }
