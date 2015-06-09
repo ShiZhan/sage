af90a03916eb131758a5590e0274aed32a48e362
ShiZhan <g.shizhan.g@gmail.com>
2015-06-09 14:40:39 +0800

    clean

--- src/main/scala/graph/Processor.scala
+++ src/main/scala/graph/Processor.scala
@@ -5,7 +5,6 @@ package graph
  * graph processer
  * algorithm: algorithm name and parameters
  * edgeFile:  input edge list
- * vdbFile:   KV file for storing immediate and final vertex value
  * nScan:     process with multiple threads
  */
 object Processor {
@@ -14,30 +13,6 @@ object Processor {
 
   def formatter(elem: (Long, Any)) = elem match { case (k: Long, v: Any) => s"$k $v" }
 
-  //  def run(edgeFile: String, vdbFile: String, nScan: Int, algorithm: String) = {
-  //    implicit val context = Context(edgeFile, vdbFile, nScan)
-  //    val a = algorithm.split(":").toList match {
-  //      case "bfs" :: root :: Nil => new BFS(root.toLong)
-  //      case "bfs" :: "u" :: root :: Nil => new BFS_U(root.toLong)
-  //      case "sssp" :: root :: Nil => new SSSP(root.toLong)
-  //      case "cc" :: Nil => new CC
-  //      case "community" :: Nil => new Community
-  //      case "cluster" :: Nil => new Cluster
-  //      case "pagerank" :: Nil => new PageRank
-  //      case "triangle" :: Nil => new Triangle
-  //      case "kcore" :: Nil => new KCore
-  //      case "degree" :: Nil => new Degree
-  //      case "degree" :: "u" :: Nil => new Degree_U
-  //      case "status" :: Nil => new Status
-  //      case _ => new Status
-  //    }
-  //
-  //    val outFile = edgeFile + "-" + algorithm.replace(':', '-') + ".out"
-  //    a.run match {
-  //      case Some(result) => result.map(formatter).toFile(outFile)
-  //      case _ =>
-  //    }
-  //  }
   def run(edgeFile: String, nScan: Int, algorithm: String) = {
     implicit val context = Context(edgeFile, nScan)
     val a = algorithm.split(":").toList match {
