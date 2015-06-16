7e68922587a948a546e3dae47bfd23ef9b1f811f
ShiZhan <g.shizhan.g@gmail.com>
2015-06-17 01:35:32 +0800

    clean

--- src/main/scala/algorithms/Algorithm.scala
+++ src/main/scala/algorithms/Algorithm.scala
@@ -1,33 +1,5 @@
 package algorithms
 
-//abstract class Algorithm[Value: Manifest](default: Value) extends helper.Logging {
-//  import scala.collection.mutable.BitSet
-//  import helper.HugeContainers._
-//
-//  val data = GrowingArray[Value](default)
-//  private var stepCounter = 0
-//  private val flags = Array.fill(2)(new BitSet)
-//  private def sFlag = flags(stepCounter & 1)
-//  private def gFlag = flags((stepCounter + 1) & 1)
-//  def scatter(id: Long, value: Value) = { val i = id.toInt; sFlag.add(i); data(id) = value }
-//  def scatter = sFlag.toIterator.map(_.toLong)
-//  def gather(id: Long) = gFlag.contains(id.toInt)
-//  def gather = gFlag.toIterator.map(_.toLong)
-//  def update = {
-//    val stat = "[ % 10d -> % 10d ]".format(gFlag.size, sFlag.size)
-//    gFlag.clear()
-//    stepCounter += 1
-//    logger.info("Step {}: {}", stepCounter, stat)
-//  }
-//
-//  def iterations: Unit
-//
-//  def run = {
-//    logger.info("Start ...")
-//    iterations
-//    data.inUse
-//  }
-//}
 abstract class Algorithm[Value: Manifest] extends helper.Logging {
   import scala.collection.mutable.BitSet
   import scala.collection.concurrent.TrieMap
@@ -37,7 +9,7 @@ abstract class Algorithm[Value: Manifest] extends helper.Logging {
   private val flags = Array.fill(2)(new BitSet)
   private def sFlag = flags(stepCounter & 1)
   private def gFlag = flags((stepCounter + 1) & 1)
-  def scatter(id: Long, value: Value) = { val i = id.toInt; sFlag.add(i); data(id) = value }
+  def scatter(id: Long, value: Value) = { sFlag.add(id.toInt); data(id) = value }
   def scatter = sFlag.toIterator.map(_.toLong)
   def gather(id: Long) = gFlag.contains(id.toInt)
   def gather = gFlag.toIterator.map(_.toLong)
