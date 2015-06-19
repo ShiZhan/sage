751603cba28bf0ff1375fcb7ab6dc649630d7d8e
ShiZhan <g.shizhan.g@gmail.com>
2015-06-19 18:31:02 +0800

    WEdge --> Edge

--- src/main/scala/graph/EdgeUtils.scala
+++ src/main/scala/graph/EdgeUtils.scala
@@ -8,6 +8,10 @@ object EdgeUtils {
   import scala.util.Random
   import graph.{ Edge, WEdge }
 
+  implicit class EdgeConverter[WEdge](edges: Iterator[WEdge]) {
+    def toEdges = edges.map { case WEdge(u, v, w) => Edge(u, v) }
+  }
+
   implicit class WEdgeConverter[Edge](edges: Iterator[Edge]) {
     def toWEdges = edges.map { case Edge(u, v) => WEdge(u, v, Random.nextFloat) }
   }
