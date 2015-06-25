66fa732741564b307820ef45045acbc53d31d6e4
ShiZhan <g.shizhan.g@gmail.com>
2015-06-25 22:47:06 +0800

    memory sequential and random access test

--- src/test/scala/sage/MemSequentialRandomTest.scala
+++ src/test/scala/sage/MemSequentialRandomTest.scala
@@ -6,33 +6,26 @@ object MemSequentialRandomTest {
   import helper.Timing._
 
   val edgeTotal = 1 << 23
+  val edgeRange = 0 to (edgeTotal - 1)
 
   def main(args: Array[String]) = {
     println(s"prepare $edgeTotal edges")
     val edgeArray = Array.fill(edgeTotal)(Edge(Random.nextInt, Random.nextInt))
     println("prepare sequential and random addresses")
-    val sAddress = (0 to (edgeTotal - 1)).toArray
+    val sAddress = edgeRange.toArray
     val rAddress = Array.fill(edgeTotal)(Random.nextInt(edgeTotal))
     println("comparing...")
-    var counter = 0
-    val esr = { () =>
-      (1 to edgeTotal).foreach { i => val Edge(u, v) = edgeArray(sAddress(i - 1)); counter += 1 }
-    }.elapsed
-    println(s"$counter sequential read,  $esr ms")
-    counter = 0
-    val err = { () =>
-      (1 to edgeTotal).foreach { i => val Edge(u, v) = edgeArray(rAddress(i - 1)); counter += 1 }
-    }.elapsed
-    println(s"$counter random     read,  $err ms")
-    counter = 0
-    val esw = { () =>
-      (1 to edgeTotal).foreach { i => edgeArray(sAddress(i - 1)) = Edge(-1, -1); counter += 1 }
-    }.elapsed
-    println(s"$counter sequential write, $esw ms")
-    counter = 0
-    val erw = { () =>
-      (1 to edgeTotal).foreach { i => edgeArray(rAddress(i - 1)) = Edge(-1, -1); counter += 1 }
-    }.elapsed
-    println(s"$counter random     write, $erw ms")
+
+    val esr = { () => edgeRange.foreach { i => val Edge(u, v) = edgeArray(sAddress(i)) } }.elapsed
+    println("%8d sequential  read, %8d ms, % 8d MB/s".format(edgeTotal, esr, (edgeTotal >> 16)*1000/esr))
+
+    val err = { () => edgeRange.foreach { i => val Edge(u, v) = edgeArray(rAddress(i)) } }.elapsed
+    println("%8d random      read, %8d ms, % 8d MB/s".format(edgeTotal, err, (edgeTotal >> 16)*1000/err))
+
+    val esw = { () => edgeRange.foreach { i => edgeArray(sAddress(i)) = Edge(-1, -1) } }.elapsed
+    println("%8d sequential write, %8d ms, % 8d MB/s".format(edgeTotal, esw, (edgeTotal >> 16)*1000/esw))
+
+    val erw = { () => edgeRange.foreach { i => edgeArray(rAddress(i)) = Edge(-1, -1) } }.elapsed
+    println("%8d random     write, %8d ms, % 8d MB/s".format(edgeTotal, erw, (edgeTotal >> 16)*1000/erw))
   }
 }
