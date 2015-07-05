e059e6250fb0c8a6477d44d0624cb33a2a86a6e9
ShiZhan <g.shizhan.g@gmail.com>
2015-07-05 20:53:20 +0800

    Long->Int in FileBufAccessTest

--- src/test/scala/sage/FileBufAccessTest.scala
+++ src/test/scala/sage/FileBufAccessTest.scala
@@ -26,7 +26,7 @@ object FileBufAccessTest {
           fc.position(0)
           edges.grouped(nEdge).foreach { g =>
             buf.clear()
-            for (Edge(u, v) <- g) { buf.putLong(u); buf.putLong(v) }
+            for (Edge(u, v) <- g) buf.putInt(u).putInt(v)
             buf.flip()
             while (buf.hasRemaining) fc.write(buf)
           }
@@ -48,8 +48,8 @@ object FileBufAccessTest {
             while (fc.read(buf) != -1 && buf.hasRemaining) {}
             buf.flip()
             while (buf.hasRemaining) {
-              val u = buf.getLong
-              val v = buf.getLong
+              val u = buf.getInt
+              val v = buf.getInt
               //Edge(u, v)
             }
           }
