de755d8ae812be284eca2b8fe868dfc3a83f504a
ShiZhan <g.shizhan.g@gmail.com>
2015-07-07 00:23:50 +0800

    diff in test

--- src/test/scala/sage/ParallelAlgorithmTest.scala
+++ src/test/scala/sage/ParallelAlgorithmTest.scala
@@ -31,23 +31,22 @@ object ParallelAlgorithmTest {
     val (r0s, e0s) = { () => a0s.run }.elapsed
     println(s"Degree 1 group: $e0s ms")
     val (r0p, e0p) = { () => a0p.run }.elapsed
-    println(s"Degree $nGroups groups: $e0p ms, ${a0s.vertices == a0p.vertices}")
+    println(s"Degree $nGroups groups: $e0p ms, diff ${(a0s.vertices <> a0p.vertices).size}")
 
     val (r1s, e1s) = { () => a1s.run }.elapsed
     println(s"BFS 1 group: $e1s ms")
     val (r1p, e1p) = { () => a1p.run }.elapsed
-    println(s"BFS $nGroups groups: $e1p ms, ${a1s.vertices == a1p.vertices}")
+    println(s"BFS $nGroups groups: $e1p ms, diff ${(a1s.vertices <> a1p.vertices).size}")
 
     val (r2s, e2s) = { () => a2s.run }.elapsed
     println(s"CC 1 group: $e2s ms")
     val (r2p, e2p) = { () => a2p.run }.elapsed
-    println(s"CC $nGroups groups: $e2p ms, ${a2s.vertices == a2p.vertices}")
+    println(s"CC $nGroups groups: $e2p ms, diff ${(a2s.vertices <> a2p.vertices).size}")
 
     val (r3s, e3s) = { () => a3s.run }.elapsed
     println(s"KCore 1 group: $e3s ms")
     val (r3p, e3p) = { () => a3p.run }.elapsed
-    println(s"KCore $nGroups groups: $e3p ms, ${a3s.vertices == a3p.vertices}")
-    println((a3s.vertices <> a3p.vertices).size)
+    println(s"KCore $nGroups groups: $e3p ms, diff ${(a3s.vertices <> a3p.vertices).size}")
 
     val (r4s, e4s) = { () => a4s.run }.elapsed
     println(s"PageRank 1 group: $e4s ms")
