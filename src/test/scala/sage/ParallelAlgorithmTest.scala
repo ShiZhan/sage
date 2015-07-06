512743bf7c3d4a1b2a85ed91160565cc9bb1527a
ShiZhan <g.shizhan.g@gmail.com>
2015-07-06 23:58:45 +0800

    parallel algorithm test with result comparison

--- src/test/scala/sage/ParallelAlgorithmTest.scala
+++ src/test/scala/sage/ParallelAlgorithmTest.scala
@@ -12,34 +12,45 @@ object ParallelAlgorithmTest {
   }
 
   def main(args: Array[String]) = {
-    val edges = new RecursiveMAT(18, 8).getEdges.toArray
+    val edges = new RecursiveMAT(12, 8).getEdges.filterNot(_.selfloop).toArray
     implicit val edgeProvider = new TestEdgeProvider(edges)
-    implicit val edgeProviders = edges.grouped(1 << 18).map(new TestEdgeProvider(_)).toSeq
-    val nGroups = 1 << (18 + 3 - 18)
+    implicit val edgeProviders = edges.grouped(1 << 12).map(new TestEdgeProvider(_)).toSeq
+    val nGroups = 1 << (12 + 3 - 12)
 
-    val (result0, e0) = { () => new Degree_U().run }.elapsed
-    println(s"Degree 1 group: $e0 ms")
-    val (result1, e1) = { () => new Degree_UP().run }.elapsed
-    println(s"Degree $nGroups groups: $e1 ms")
+    val a0s = new Degree_U()
+    val a0p = new Degree_UP()
+    val a1s = new BFS_U(0)
+    val a1p = new BFS_UP(0)
+    val a2s = new CC()
+    val a2p = new CC_P()
+    val a3s = new KCore()
+    val a3p = new KCore_P()
+    val a4s = new PageRank(10)
+    val a4p = new PageRank_P(10)
 
-    val (result2, e2) = { () => new BFS_U(0).run }.elapsed
-    println(s"BFS 1 group: $e2 ms")
-    val (result3, e3) = { () => new BFS_UP(0).run }.elapsed
-    println(s"BFS $nGroups groups: $e3 ms")
+    val (r0s, e0s) = { () => a0s.run }.elapsed
+    println(s"Degree 1 group: $e0s ms")
+    val (r0p, e0p) = { () => a0p.run }.elapsed
+    println(s"Degree $nGroups groups: $e0p ms, ${a0s.vertices == a0p.vertices}")
 
-    val (result4, e4) = { () => new CC().run }.elapsed
-    println(s"CC 1 group: $e4 ms")
-    val (result5, e5) = { () => new CC_P().run }.elapsed
-    println(s"CC $nGroups groups: $e5 ms")
+    val (r1s, e1s) = { () => a1s.run }.elapsed
+    println(s"BFS 1 group: $e1s ms")
+    val (r1p, e1p) = { () => a1p.run }.elapsed
+    println(s"BFS $nGroups groups: $e1p ms, ${a1s.vertices == a1p.vertices}")
 
-    val (result6, e6) = { () => new KCore().run }.elapsed
-    println(s"KCore 1 group: $e6 ms")
-    val (result7, e7) = { () => new KCore_P().run }.elapsed
-    println(s"KCore $nGroups groups: $e7 ms")
+    val (r2s, e2s) = { () => a2s.run }.elapsed
+    println(s"CC 1 group: $e2s ms")
+    val (r2p, e2p) = { () => a2p.run }.elapsed
+    println(s"CC $nGroups groups: $e2p ms, ${a2s.vertices == a2p.vertices}")
 
-    val (result8, e8) = { () => new PageRank(10).run }.elapsed
-    println(s"PageRank 1 group: $e8 ms")
-    val (result9, e9) = { () => new PageRank_P(10).run }.elapsed
-    println(s"PageRank $nGroups groups: $e9 ms")
+    val (r3s, e3s) = { () => a3s.run }.elapsed
+    println(s"KCore 1 group: $e3s ms")
+    val (r3p, e3p) = { () => a3p.run }.elapsed
+    println(s"KCore $nGroups groups: $e3p ms, ${a3s.vertices == a3p.vertices}")
+
+    val (r4s, e4s) = { () => a4s.run }.elapsed
+    println(s"PageRank 1 group: $e4s ms")
+    val (r4p, e4p) = { () => a4p.run }.elapsed
+    println(s"PageRank $nGroups groups: $e4p ms")
   }
 }
