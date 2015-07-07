c33b68cea5c4bedb270cf267129a6ae6f2a1ca7f
ShiZhan <g.shizhan.g@gmail.com>
2015-07-07 19:10:14 +0800

    archive

--- src/main/scala/generators/RMAT.scala
+++ src/main/scala/generators/RMAT.scala
@@ -1,23 +1,28 @@
 package generators
 
+import java.util.concurrent.ThreadLocalRandom
+import scala.collection.JavaConversions._
 import graph.{ Edge, SimpleEdge, EdgeProvider }
 
 class RecursiveMAT(scale: Int, degree: Long) extends EdgeProvider[SimpleEdge] {
   require(scale > 0 && scale < 31 && degree > 0)
-  import java.util.concurrent.ThreadLocalRandom
 
-  val totalEdges = (1L << scale) * degree
+  val E = (1L << scale) * degree
   val edgeIDs = {
     var eID = 0L
-    Iterator.continually(eID).takeWhile { _ => eID += 1; eID <= totalEdges }
+    Iterator.continually(eID).takeWhile { _ => eID += 1; eID <= E }
   }
 
-  def dice(d: Int) =
-    if (d < 57) (0, 0) else if (d < 76) (1, 0) else if (d < 95) (0, 1) else (1, 1)
+  def dice(i: Int) = i match {
+    case d if (d < 57) => (0, 0)
+    case d if (d < 76) => (1, 0)
+    case d if (d < 95) => (0, 1)
+    case _ => (1, 1)
+  }
 
   def nextEdge = {
-    val tlRandom = ThreadLocalRandom.current()
-    val dices = Seq.fill(scale)(tlRandom.nextInt(100)).map(dice)
+    val r = ThreadLocalRandom.current()
+    val dices = r.ints(scale, 0, 99).iterator().map(_.toInt).map(dice)
     val (u, v) = ((0, 0) /: dices) { (p0, p1) =>
       val (x0, y0) = p0; val (x1, y1) = p1
       ((x0 << 1) + x1, (y0 << 1) + y1)
@@ -26,5 +31,5 @@ class RecursiveMAT(scale: Int, degree: Long) extends EdgeProvider[SimpleEdge] {
   }
 
   def getEdges =
-    edgeIDs.grouped(1 << 13).map(_.par.map { _ => nextEdge }.toIterator).flatten
+    edgeIDs.grouped(1 << 13).flatMap { _.par.map { _ => nextEdge }.toIterator }
 }
