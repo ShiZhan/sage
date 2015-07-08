b60e0c6a3fea5fba4e6ac933bcc9160679f79be6
ShiZhan <g.shizhan.g@gmail.com>
2015-07-08 12:08:18 +0800

    compare and confirm parallel collection speedup

--- src/test/scala/sage/ParallelIterationTest.scala
+++ src/test/scala/sage/ParallelIterationTest.scala
@@ -15,14 +15,21 @@ object ParallelIterationTest {
 
   val r = 1 to 1 << 3
 
-  def generic = () => for (i <- r) yield someOps
-
-  def parallel = () => for (i <- r.par) yield someOps
+  def batchOps0 = () => for (i <- r) yield someOps
+  def batchOps1 = () => for (i <- r.par) yield someOps
+  def batchOps2 = () => r.par.map(_ => someOps)
+  def batchOps3 = () => (1 to 1 << 4).par.map(_ => someOps)
 
   def main(args: Array[String]) = {
-    val (result0, e0) = { generic }.elapsed
-    println(s"$result0, $e0")
-    val (result1, e1) = { parallel }.elapsed
-    println(s"$result1, $e1")
+    println("items  elapsed (ms)")
+    println("-----  ------------")
+    val (result0, e0) = { batchOps0 }.elapsed
+    println("% 5d  % 12d".format(result0.size, e0))
+    val (result1, e1) = { batchOps1 }.elapsed
+    println("% 5d  % 12d".format(result1.size, e1))
+    val (result2, e2) = { batchOps2 }.elapsed
+    println("% 5d  % 12d".format(result2.size, e2))
+    val (result3, e3) = { batchOps3 }.elapsed
+    println("% 5d  % 12d".format(result3.size, e3))
   }
 }
