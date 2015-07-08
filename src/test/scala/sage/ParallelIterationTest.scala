5a02487508ba253abebab93872b2aa659d19ebd6
ShiZhan <g.shizhan.g@gmail.com>
2015-07-08 10:49:57 +0800

    parallel iteration test

---
+++ src/test/scala/sage/ParallelIterationTest.scala
@@ -0,0 +1,28 @@
+package sage.test
+
+object ParallelIterationTest {
+  import java.util.concurrent.ThreadLocalRandom
+  import java.security.MessageDigest
+  import helper.Timing._
+
+  def someOps = {
+    val md = MessageDigest.getInstance("SHA1")
+    val bytes = Array.fill(1 << 25)(0.toByte)
+    ThreadLocalRandom.current().nextBytes(bytes)
+    md.update(bytes)
+    md.digest().map("%02x".format(_)).mkString
+  }
+
+  val r = 1 to 1 << 3
+
+  def generic = for (i <- r) yield someOps
+
+  def parallel = for (i <- r.par) yield someOps
+
+  def main(args: Array[String]) = {
+    val (result0, e0) = { () => generic }.elapsed
+    println(s"$result0, $e0")
+    val (result1, e1) = { () => parallel }.elapsed
+    println(s"$result1, $e1")
+  }
+}
