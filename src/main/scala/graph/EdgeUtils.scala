d08474e0c87b00ed0a867de3990f164540fa91e7
ShiZhan <g.shizhan.g@gmail.com>
2015-07-09 10:12:02 +0800

    comments

--- src/main/scala/graph/EdgeUtils.scala
+++ src/main/scala/graph/EdgeUtils.scala
@@ -3,6 +3,8 @@ package graph
 /**
  * @author Zhan
  * Edge utilities
+ * edge2wedge: convert simple edge to weighted edge
+ * wedge2edge: convert weighted edge to simple edge
  */
 object EdgeUtils {
   def edge2wedge(e: SimpleEdge) = Edge(e.from, e.to, util.Random.nextFloat)
