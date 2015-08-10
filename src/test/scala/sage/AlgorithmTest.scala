c815d6cb4cc2e5458e9b42d56b2301bd9b5f3133
ShiZhan <g.shizhan.g@gmail.com>
2015-08-10 17:48:36 +0800

    ...

--- src/test/scala/sage/AlgorithmTest.scala
+++ src/test/scala/sage/AlgorithmTest.scala
@@ -11,16 +11,15 @@ object AlgorithmTest {
   def main(args: Array[String]) =
     if (args.nonEmpty) {
       val engine = new Engine(args)
-      //val engine_w = new Engine_W(args)
-      //val algorithms = Seq(
-      //  new BFS(0), new BFS_U(0),
-      //  new SSSP(0), new SSSP_U(0),
-      //  new CC(),
-      //  new Degree(), new Degree_U(),
-      //  new KCore(),
-      //  new PageRank(10))
+      val algorithms = Seq(
+        new BFS_U(0),
+        new SSSP_U(0),
+        new CC(),
+        new Degree_U(),
+        new KCore(),
+        new PageRank(10))
 
-      engine.run(new PageRank(10))
+      engine.run(algorithms(5))
 
       //for (a <- algorithms) {
       //  val (r, e) = { () => a.run }.elapsed
