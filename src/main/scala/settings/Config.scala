409584604af394de0b59fcbcaa7d2d327219b395
ShiZhan <g.shizhan.g@gmail.com>
2015-08-15 10:41:55 +0800

    default configuration

--- src/main/scala/settings/Config.scala
+++ src/main/scala/settings/Config.scala
@@ -5,9 +5,13 @@ package settings
  * parsing config file
  */
 object Config {
-  import com.typesafe.config.ConfigFactory
+  import java.io.File
+  import com.typesafe.config.{ Config, ConfigFactory }
 
-  val config = ConfigFactory.parseResources("sage.conf")
+  val config = ConfigFactory.parseFile(new File("sage.conf")) match {
+    case c: Config if c.isEmpty() => ConfigFactory.parseResources("sage.conf")
+    case c: Config => c
+  }
   val nBuffersPerScanner = config.getInt("buffer.nBuffersPerScanner")
   val nEdgesPerBuffer = config.getInt("buffer.nEdgesPerBuffer")
 }
