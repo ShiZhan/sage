68668315e9737c56342f8c85a451c60d9dbcb29c
ShiZhan <g.shizhan.g@gmail.com>
2015-08-15 13:46:06 +0800

    grid generator simplification

--- src/main/scala/graph/generators/Grid.scala
+++ src/main/scala/graph/generators/Grid.scala
@@ -2,45 +2,38 @@ package graph.generators
 
 import graph.{ Edge, SimpleEdge, EdgeProvider }
 
-class Grid2(rScale: Int, cScale: Int) extends EdgeProvider[SimpleEdge] {
-  require(rScale > 0 && rScale < 30 && cScale > 0 && cScale < 30)
-  val row = 1 << rScale
-  val col = 1 << cScale
+class Grid2(xScale: Int, yScale: Int) extends EdgeProvider[SimpleEdge] {
+  require(xScale > 0 && xScale < 30 && yScale > 0 && yScale < 30)
+
+  val X = 1 << xScale
+  val Y = 1 << yScale
 
   def sequence(size: Int) = Iterator.from(0).take(size)
+  def id(x: Int, y: Int) = (x << yScale) + y
 
-  def getEdges =
-    sequence(row).flatMap { r =>
-      sequence(col).flatMap { c =>
-        val id = (r << cScale) + c
-        val idH = (r << cScale) + (c + 1 & col - 1)
-        val idV = (((r + 1) & (row - 1)) << cScale) + c
-        Iterator(Edge(id, idH), Edge(id, idV))
-      }
-    }
+  def getEdges = for (
+    x <- sequence(X);
+    y <- sequence(Y);
+    u = id(x, y);
+    v <- Iterator(id(x + 1 & X - 1, y), id(x, y + 1 & Y - 1))
+  ) yield Edge(u, v)
 }
 
 class Grid3(xScale: Int, yScale: Int, zScale: Int) extends EdgeProvider[SimpleEdge] {
-  require(xScale > 0 && xScale < 20
-    && yScale > 0 && yScale < 20
-    && zScale > 0 && zScale < 20)
+  require(xScale > 0 && xScale < 20 && yScale > 0 && yScale < 20 && zScale > 0 && zScale < 20)
 
   val X = 1 << xScale
   val Y = 1 << yScale
   val Z = 1 << zScale
 
   def sequence(size: Int) = Iterator.from(0).take(size)
+  def id(x: Int, y: Int, z: Int) = (x << (yScale + zScale)) + (y << zScale) + z
 
-  def getEdges =
-    sequence(X).flatMap { x =>
-      sequence(Y).flatMap { y =>
-        sequence(Z).flatMap { z =>
-          val id = (x << (yScale + zScale)) + (y << zScale) + z
-          val idX = ((x + 1 & X - 1) << (yScale + zScale)) + (y << zScale) + z
-          val idY = (x << (yScale + zScale)) + ((y + 1 & Y - 1) << zScale) + z
-          val idZ = (x << (yScale + zScale)) + (y << zScale) + (z + 1 & Z - 1)
-          Iterator(Edge(id, idX), Edge(id, idY), Edge(id, idZ))
-        }
-      }
-    }
+  def getEdges = for (
+    x <- sequence(X);
+    y <- sequence(Y);
+    z <- sequence(Z);
+    u = id(x, y, z);
+    v <- Iterator(id(x + 1 & X - 1, y, z), id(x, y + 1 & Y - 1, z), id(x, y, z + 1 & Z - 1))
+  ) yield Edge(u, v)
 }
