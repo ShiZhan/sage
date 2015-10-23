895a92e121bc70bdf4140c205070604f4eeb6016
ShiZhan <g.shizhan.g@gmail.com>
2015-10-23 09:56:36 +0800

    [BUG FIX] pagerank GC

--- src/main/scala/graph/algorithms/PageRank.scala
+++ src/main/scala/graph/algorithms/PageRank.scala
@@ -3,39 +3,33 @@ package graph.algorithms
 import graph.{ Edge, SimpleEdge }
 import graph.Parallel.Algorithm
 import helper.GrowingArray
-import helper.Lines.LinesWrapper
 
-case class PRValue(value: Float, sum: Float, deg: Int) {
-  def addDeg = PRValue(value, sum, deg + 1)
-  def initPR(implicit nVertex: Int) = PRValue(1 / nVertex, 0.0f, deg)
-  def gather(delta: Float) = PRValue(value, sum + delta, deg)
-  def scatter = value / deg
-  def update(implicit nVertex: Int) = PRValue(0.15f / nVertex + sum * 0.85f, 0.0f, deg)
-  override def toString = "%f".format(value)
-}
-
-class PageRank(nLoop: Int) extends Algorithm[SimpleEdge, PRValue](PRValue(0.0f, 0.0f, 0)) {
-  implicit lazy val nVertex = vertices.nUpdated
+class PageRank(nLoop: Int) extends Algorithm[SimpleEdge, Float](0.0f) {
+  val deg = GrowingArray[Int](0)
+  val sum = GrowingArray[Float](0.0f)
+  val flg = flags(0)
+  lazy val nVertex = vertices.nUpdated
 
   override def forward() = stepCounter += 1
   override def hasNext() = stepCounter <= nLoop
 
   def compute(edges: Iterator[SimpleEdge]) = if (stepCounter == 0) {
     for (Edge(u, v) <- edges) {
-      vertices(u) = vertices(u).addDeg
-      vertices(v) = vertices(v).initPR(1)
+      deg(u) = deg(u) + 1
+      flg.add(u)
+      flg.add(v)
     }
   } else {
     for (Edge(u, v) <- edges) vertices.synchronized {
-      vertices(v) = vertices(v).gather(vertices(u).scatter)
+      sum(v) = sum(v) + vertices(u) / deg(u)
     }
   }
 
   def update() = if (stepCounter == 0) {
     logger.info("initialize PR value")
-    for ((id, value) <- vertices.updated) vertices(id) = value.initPR
+    for ((id, value) <- vertices.updated) vertices(id) = 0.15f / nVertex
   } else {
     logger.info("update PR value")
-    for ((id, value) <- vertices.updated) vertices(id) = value.update
+    for (id <- flg) vertices(id) = 0.15f / nVertex + sum(id) * 0.85f
   }
 }
